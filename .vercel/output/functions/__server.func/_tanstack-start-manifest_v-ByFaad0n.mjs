204e2ec23cc7569e484b1a08bbeb8bf2dc5688d0
VictorWest <victorwaribokowest@gmail.com>
2026-06-04 06:57:05 -0400

    added nitro

---
+++ .vercel/output/functions/__server.func/_tanstack-start-manifest_v-ByFaad0n.mjs
@@ -0,0 +1,4 @@
+const tsrStartManifest = () => ({ routes: { __root__: { filePath: "C:/Users/USER/Desktop/Portfolio/My Developer Story/src/routes/__root.tsx", children: ["/"], preloads: ["/assets/index-kH_fCldT.js"], scripts: [{ attrs: { type: "module", async: true, src: "/assets/index-kH_fCldT.js" } }] }, "/": { filePath: "C:/Users/USER/Desktop/Portfolio/My Developer Story/src/routes/index.tsx", children: void 0, preloads: ["/assets/index-W7YVYrhN.js"] } } });
+export {
+  tsrStartManifest
+};
